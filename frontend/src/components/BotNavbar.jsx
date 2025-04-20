94e42260f10e10d829e79ddbc34712d0b9d64cb2
Ryad <ryadmansouri04@gmail.com>
2025-04-20 19:45:33 +0100

    V9.2

--- frontend/src/components/BotNavbar.jsx
+++ frontend/src/components/BotNavbar.jsx
@@ -6,15 +6,15 @@ const BotNavbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
   return (
     <div className='fixed top-[80px] left-0 right-0 z-[999] botnavbar py-3 px-[3vw] sm:px-[3vw] md:px-[7vw] lg:px-[9vw] bg-primary'>
-      <div className='hidden md:flex max-lg:gap-x-1 text-gray-100 items-center gap-2 justify-center'>
-        <NavLink to={"/"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Home</NavLink>
-        <NavLink to={"/shop"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Shop</NavLink>
-        <NavLink to={"/vetement"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Vetement</NavLink>
-        <NavLink to={"/decoration"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Decoration</NavLink>
+      <div className='hidden font-inter font-medium md:flex max-lg:gap-x-1 text-third items-center gap-3 justify-center'>
+        <NavLink to={"/"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Accueil</NavLink>
+        <NavLink to={"/shop"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Magasin</NavLink>
+        <NavLink to={"/vetement"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Vêtements</NavLink>
+        <NavLink to={"/decoration"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Décoration</NavLink>
         <NavLink to={"/cuisine"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Cuisine</NavLink>
-        <NavLink to={"/accessoire"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Accessoire</NavLink>
+        <NavLink to={"/accessoire"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Accessoires</NavLink>
         <NavLink to={"/bijoux"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Bijoux</NavLink>
-        <NavLink to={"/bain-beaute"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Bain et Beaute</NavLink>
+        <NavLink to={"/bain-beaute"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Bain et Beauté</NavLink>
         <NavLink to={"/about-us"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">About Us</NavLink>
         <NavLink to={"/contact-us"} className="py-1 px-2 transition-all duration-300 max-lg:text-sm">Contact Us</NavLink>
       </div>
@@ -23,14 +23,14 @@ const BotNavbar = () => {
         <RiMenu3Fill className='text-white text-3xl cursor-pointer' onClick={() => { setShowNavbar((prev) => !prev); }} />
         {/* Links */}
         <div className={`overflow-hidden botnavbar-responsive ${showNavbar ? "w-full" : "w-0"} pb-5 absolute top-[100%] py-3 right-0 transition-all duration-300 bg-second border-t border-gray-800 flex flex-col gap-2 z-[9000] text-gray-100`}>
-          <NavLink to={"/"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Home</NavLink>
-          <NavLink to={"/shop"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Shop</NavLink>
-          <NavLink to={"/vetement"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Vetement</NavLink>
-          <NavLink to={"/decoration"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Decoration</NavLink>
+          <NavLink to={"/"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Accueil</NavLink>
+          <NavLink to={"/shop"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Magasin</NavLink>
+          <NavLink to={"/vetement"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Vêtements</NavLink>
+          <NavLink to={"/decoration"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Décoration</NavLink>
           <NavLink to={"/cuisine"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Cuisine</NavLink>
-          <NavLink to={"/accessoire"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Accessoire</NavLink>
+          <NavLink to={"/accessoire"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Accessoires</NavLink>
           <NavLink to={"/bijoux"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Bijoux</NavLink>
-          <NavLink to={"/bain-beaute"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Bain et Beaute</NavLink>
+          <NavLink to={"/bain-beaute"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Bain et Beauté</NavLink>
           <NavLink to={"/about-us"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>About Us</NavLink>
           <NavLink to={"/contact-us"} className="py-3 px-2 transition-all duration-300" onClick={() => { setShowNavbar(false); scrollTo(0, 0); }}>Contact Us</NavLink>
         </div>
